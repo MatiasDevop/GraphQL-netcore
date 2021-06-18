6bab74464776dcebc5db14f9537aebf2cc89e68c
Nestor Matias <solomatias321@gmail.com>
2021-06-18 13:10:01 +0200

    adding second table and set foreing keys

--- Models/Platform.cs
+++ Models/Platform.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
 namespace CommanderGQL.Models
@@ -10,5 +11,6 @@ namespace CommanderGQL.Models
         [Required]
         public string Name { get; set; }
         public string LicenceKey { get; set; }
+        public ICollection<Command> Commands { get; set; } = new List<Command>();
     }
 }
